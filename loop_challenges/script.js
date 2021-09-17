9354732ee00a3ed83011a814a9ff94455537e5a7
Devin Kearns <devin.kearns@outlook.com>
2021-09-17 13:36:38 -0400

    Loop Challenge Solutions

---
+++ loop_challenges/script.js
@@ -0,0 +1,39 @@
+
+// Print odds 1-20
+for(var i=1; i<=20; i++){
+    if(i % 2 !=0){
+        console.log(i);
+    }
+}
+console.log(" ")
+
+
+// Decreasing Muliptles of 3
+for(var j=100; j>=0; j--){
+    if (j % 3 == 0){
+        console.log(j);
+    }
+}
+console.log(" ")
+
+//print the sequence
+for(var i = 4; i >= -3.5; i -= 1.5){
+    console.log(i);
+}
+console.log(" ")
+
+// Sigma
+var sum = 0;
+for(var i=1; i<=100; i++){
+    sum += i;
+}
+console.log(sum);
+console.log(" ")
+
+//Fractorial
+var product = 1;
+for(i=1; i<=12; i++){
+    product *= i;
+}
+console.log(product);
+
