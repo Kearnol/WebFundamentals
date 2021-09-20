2c736d2cc46e8c70f9a4a95b2dbb02d478ba8c34
Devin Kearns <devin.kearns@outlook.com>
2021-09-20 12:49:52 -0400

    Pizza Oven

---
+++ Dojo_Pizzeria/script.js
@@ -0,0 +1,15 @@
+function pizzaOven(crustType, suaceType, cheeses, toppings){
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.suaceType = suaceType;
+    pizza.cheese = cheeses;
+    pizza.toppings = toppings;
+    return pizza
+    }
+
+var pizza1 = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"])
+var pizza2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olvies", "onions"])
+var pizza3 = pizzaOven("thin crust", "white sauces", ["white chees", "parm"], ["onions", "peppers"]);
+var pizza4 = pizzaOven("regular crust", "marinara", ["mozzeralla"], ["BBQ Chicken", "Bacon", "onions"]);
+
+console.log(pizza1, pizza2, pizza3, pizza4);
