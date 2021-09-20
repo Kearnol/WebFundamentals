8e8d748e0021231bb5dcfaac39dc1c3e9fe0e556
Devin Kearns <devin.kearns@outlook.com>
2021-09-20 12:56:10 -0400

    Added bonus challenge

--- Dojo_Pizzeria/script.js
+++ Dojo_Pizzeria/script.js
@@ -12,4 +12,16 @@ var pizza2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mush
 var pizza3 = pizzaOven("thin crust", "white sauces", ["white chees", "parm"], ["onions", "peppers"]);
 var pizza4 = pizzaOven("regular crust", "marinara", ["mozzeralla"], ["BBQ Chicken", "Bacon", "onions"]);
 
-console.log(pizza1, pizza2, pizza3, pizza4);
+// console.log(pizza1, pizza2, pizza3, pizza4);
+
+
+// A function for random menu selection: 
+
+function randomPizza(){
+    var menu = [pizza1, pizza2, pizza3, pizza4];
+    var random = Math.floor(Math.random()* menu.length)
+    console.log(random);
+    return menu[random];
+}
+
+console.log(randomPizza());
