5f8804e92a38ea136ed373e94dd9e077c456f589
Devin Kearns <devin.kearns@outlook.com>
2021-09-21 19:07:31 -0400

    inital commit

---
+++ Array_Challenges/script.js
@@ -0,0 +1,73 @@
+function alwaysHungry(arr) {
+    for(i=0; i < arr.length; i++){
+        var food = false
+        if(arr[i] == "food"){
+            console.log("yummy");
+            food = true;
+        }
+    }
+        if(food == false){
+            console.log("I'm Hungry")
+        }
+}
+
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
+
+
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for(i=0; i<arr.length; i++){
+        if(arr[i]>cutoff){
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
+
+function betterThanAverage(arr) {
+    var sum = 0;
+    var count = 0;
+    for(i=0; i<arr.length; i++){
+        sum += arr[i];
+    }
+    var avg = sum/arr.length;
+    for(j=0; j<arr.length; j++){
+        if(arr[j] > avg){
+            count++;
+        }
+    }
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
+
+
+function reverse(arr) {
+    for(var i = 0; i < arr.length; i++){
+        var temp = arr[i];
+        arr[i] = arr[arr.length-1-i];
+        arr[arr.length-1-i] = temp;
+    }
+    return arr;
+}
+
+var result = reverse(["a", "b", "c", "d", "e"]);
+console.log(result); // we expect back ["e", "d", "c", "b", "a"]
+
+function fibonacciArray(n) {
+    // the [0, 1] are the starting values of the array to calculate the rest from
+    var fibArr = [0, 1];
+    for (i = 0; i < n-2; i++) {
+        fibArr.push((fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2]));
+        console.log(fibArr);
+    }
+    return fibArr;
+}
+var result = fibonacciArray(10);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
